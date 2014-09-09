f448dbcf72cf86a3e8022969e168a0fbc20a36c9
Alexandre Courbot <acourbot@nvidia.com>
2014-09-09 17:17:26 +0900

    Add script to download the right Linaro toolchain and use it for compilation

--- env.sh
+++ env.sh
@@ -3,11 +3,12 @@ if [[ -z "$TOP" ]]; then
     exit 1
 fi
 
-if [[ -z "$CROSS_COMPILE" ]]; then
-    echo "Error: CROSS_COMPILE env var not set."
-    exit 1
-fi
+LINARO_GCC_VERSION=4.8
+LINARO_GCC_RELEASE=14.04
+LINARO_GCC_PACKAGE=gcc-linaro-arm-linux-gnueabihf-${LINARO_GCC_VERSION}-20${LINARO_GCC_RELEASE}_linux
 
+PATH="$TOP/$LINARO_GCC_PACKAGE/bin:$PATH"
+CROSS_COMPILE=arm-linux-gnueabihf-
 PREFIX=/home/ubuntu/usr
 export DESTDIR=$TOP/out/root
 export PKG_CONFIG_PATH="$TOP/out/root/usr/lib/pkgconfig:$DESTDIR/$PREFIX/lib/pkgconfig"
