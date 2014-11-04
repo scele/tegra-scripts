7968ec9c4b8ee07b336af429ab9f5de2aecc6fd6
Alexandre Courbot <acourbot@nvidia.com>
2014-11-04 16:10:55 +0900

    Tune PKG_CONFIG paths

--- env.sh
+++ env.sh
@@ -75,9 +75,11 @@ PATH="$TOP/$LINARO_GCC_PACKAGE/bin:$PATH"
 export SYSROOT="$TOP/out/target/$DISTRO"
 export CFLAGS="--sysroot=$SYSROOT"
 export CXXFLAGS="$CFLAGS"
-export PKG_CONFIG_SYSROOT_DIR=$SYSROOT
-export PKG_CONFIG_LIBDIR="$SYSROOT/usr/lib/pkgconfig:$SYSROOT/usr/share/pkgconfig"
-export PKG_CONFIG_PATH="$SYSROOT/$NV_PREFIX/lib/pkgconfig"
+
+export PKG_CONFIG_DIR=
+export PKG_CONFIG_LIBDIR="${SYSROOT}/usr/lib/arm-linux-gnueabihf/pkgconfig":"${SYSROOT}/usr/lib/pkgconfig":"${SYSROOT}/usr/share/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="${SYSROOT}"
+export PKG_CONFIG_PATH="${SYSROOT}/${NV_PREFIX}/lib/pkgconfig"
 # Use system pkg-config as the toolchain's one is too old (0.25) to handle PKG_CONFIG_SYSROOT_DIR correctly.
 export PKG_CONFIG=pkg-config
 ####
