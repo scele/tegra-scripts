3490f6cecdafc20b71e50db84998dccf16b414e8
Alexandre Courbot <acourbot@nvidia.com>
2014-11-05 12:19:05 +0900

    Remove unneeded scripts

--- nouveau-env.sh
+++
@@ -1,6 +0,0 @@
-NVD=$HOME/usr
-LD_LIBRARY_PATH=$NVD/lib
-PKG_CONFIG_PATH=$NVD/lib/pkgconfig/:$NVD/share/pkgconfig/
-ACLOCAL="aclocal -I $NVD/share/aclocal"
-export NVD LD_LIBRARY_PATH PKG_CONFIG_PATH ACLOCAL
-export PATH="$NVD/bin:$PATH"
